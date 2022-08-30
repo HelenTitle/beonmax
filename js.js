d330808a89322b43d62f7c4c1360b6986883f457
HelenTitle <57812036+HelenTitle@users.noreply.github.com>
2022-08-30 23:13:29 +0300

    add funtion start

--- js.js
+++ js.js
@@ -1,5 +1,15 @@
-let money = prompt ("Ваш бюджет на месяц", '');
-time = prompt ('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+function start(){
+    money = +prompt ("Ваш бюджет на месяц", '');
+    time = prompt ('Введите дату в формате YYYY-MM-DD', '');
+    while(isNaN(money)||money === ""|| money === null){
+        money = +prompt ("Ваш бюджет на месяц", '');
+    }
+
+}
+
+
 
 
 let appData = {
@@ -15,5 +25,11 @@ for (let i =0; i<2; i++){
     let b = prompt ("Во сколько это обойдется?",'');
     if ((typeof(a))==='string'&& (typeof(a))!=null && (typeof(b))!=null && a!='' && b!=''&& a.length<50) {
     appData.expenses[a] = b;
+} else{
+    i=i-1
 }
 }
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневый бюджет: " + appData.moneyPerDay);
