f852895fdddd9b866a32cbf2f87ff92ba6bcd832
HelenTitle <57812036+HelenTitle@users.noreply.github.com>
2022-08-31 22:25:58 +0300

    functions js

--- js.js
+++ js.js
@@ -1,35 +1,54 @@
 let money, time;
 
-function start(){
-    money = +prompt ("Ваш бюджет на месяц", '');
-    time = prompt ('Введите дату в формате YYYY-MM-DD', '');
-    while(isNaN(money)||money === ""|| money === null){
-        money = +prompt ("Ваш бюджет на месяц", '');
-    }
-
+function start() {
+  money = +prompt("Ваш бюджет на месяц", "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+  while (isNaN(money) || money === "" || money === null) {
+    money = +prompt("Ваш бюджет на месяц", "");
+  }
 }
 
-
-
+start();
 
 let appData = {
-budget : money,
-expenses : {},
-optinalExpenses : {},
-timeData: time,
-savings: false
+  budget: money,
+  expenses: {},
+  optinalExpenses: {},
+  timeData: time,
+  savings: true,
 };
 
-for (let i =0; i<2; i++){
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", '');
-    let b = prompt ("Во сколько это обойдется?",'');
-    if ((typeof(a))==='string'&& (typeof(a))!=null && (typeof(b))!=null && a!='' && b!=''&& a.length<50) {
+function chooseExpenses(){ 
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+  let b = prompt("Во сколько это обойдется?", "");
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
     appData.expenses[a] = b;
-} else{
-    i=i-1
+  } else {
+    i = i - 1;
+  }
 }
 }
 
-appData.moneyPerDay = appData.budget/30;
+chooseExpenses();
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Ежедневый бюджет: " + appData.moneyPerDay);
+
+
+function checkSavings(){
+    if (appData.savings === true){
+        let save = +prompt("Какова сумма накоплений?")
+        let percent = +prompt ("Под какой процент?")
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего депозита " + appData.monthIncome)
+    }
+}
+checkSavings();
