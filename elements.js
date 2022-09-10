77ef6114a45fdb25e55b6f45f0714c2f13da0a8c
HelenTitle <57812036+HelenTitle@users.noreply.github.com>
2022-09-11 00:20:08 +0300

    change the text

--- elements.js
+++ elements.js
@@ -2,7 +2,9 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.wrapper .heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('wrapper');
+
 
     box.style.backgroundColor = 'blue';
     btn[1].style.borderRadius = "100%";
@@ -24,7 +26,20 @@ let box = document.getElementById('box'),
 
     console.log(div);
 
-    document.body.appendChild(div);
+   // document.body.appendChild(div);
+
+   //wrapper.appendChild(div);
+
+   document.body.insertBefore(div, circle[0]);
+
+   document.body.removeChild(circle[1]);
+
+   wrapper.removeChild(heart[2]);
+
+   document.body.replaceChild(btn[1], circle[1]);
+
+  // div.innerHTML = '<h1>Hello world</h1>';
+  div.textContent = 'Hello world!';
 
     
 
